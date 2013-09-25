f216584d37c1c2e880549ff75456d8920a7395b2
Stefano Balietti <futur.dorko@gmail.com>
2013-09-25 11:16:11 +0200

    first commit

---
+++ js/search.js
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * Javascript support of the search interface.
+ */
+jQuery(document).ready(function(){
+    var counter = 0; var ids = "";
+    (function worker() {
+        $.ajax({
+            url: '?q=qscience_search/get_result',
+            data: { 'query_id': Drupal.settings.query_id, 'ids': ids },
+            type: 'POST',
+            success: function(data) {
+                ids = data.ids;
+	        $("#qscience_d2dsearch_results").append(data.new_results);
+	        counter += 5;
+	        $("#qscience_d2dsearch_progress")
+                    .html("This search " + counter + "% done.");
+            },
+            error: function() {
+                //alert('failure');
+	        counter = 100;
+	        $("#qscience_d2dsearch_progress")
+                    .html("This search " + counter + "% done.");
+	    },
+            complete: function() {
+                // Schedule the next request when the current one's complete
+	        if (counter < 100) {
+      	            setTimeout(worker, 500);
+	        }
+            }
+        });
+    })()
+});
